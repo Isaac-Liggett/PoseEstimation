f9b60571a27fdf298d324aba9c30690f35a56beb
Isaac-Liggett <isaacliggett19@protonmail.com>
2025-08-16 00:59:32 +1000

    added monitor route.

--- server.py
+++ server.py
@@ -12,6 +12,10 @@ def index():
 def camera():
     return render_template("camera.html")
 
+@app.route("/monitor")
+def monitor():
+    return render_template("monitor.html")
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
     # socketio.run(app, host="127.0.0.1", port=5000, debug=True)
